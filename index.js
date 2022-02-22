dd08fdcb0e06a5c3c44ae7d37b0f97308246bfaf
reyolan <reynardolan@gmail.com>
2022-02-22 18:32:19 +0800

    finish responsive to-do list

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function presentGreeting(hour) {
 	const greeting = document.querySelector("#greeting");
 	if (hour >= 0 && hour < 12) {
 		greeting.textContent = "morning";
-	} else if (hour >= 12 && hour <= 16) {
+	} else if (hour >= 12 && hour <= 18) {
 		greeting.textContent = "afternoon";
 	} else {
 		greeting.textContent = "evening";
@@ -122,12 +122,56 @@ function increaseInputWidth(e) {
 		e.target.style.width = `${(widthUnit += 1)}ch`;
 	}
 
-	if (e.key === "Backspace" && widthUnit > 1 && e.target.readOnly === false) {
+	if (e.key === "Backspace" && widthUnit > 2 && e.target.readOnly === false) {
 		e.target.style.width = `${(widthUnit -= 1)}ch`;
 	}
 }
 
-//To-do List Constructor
+//To-do List
+const toDoInput = document.querySelector("#todo-input");
+const toDoContent = document.querySelector("#todo-content");
+const ul = document.querySelector("#todo-content>ul");
+
+toDoInput.addEventListener("keydown", enterToDoInput);
+
+function enterToDoInput(e) {
+	if (e.key === "Enter" && e.target.value) {
+		addListToDom(e.target.value);
+		e.target.value = "";
+	}
+}
+
+function addListToDom(toDoText) {
+	const li = document.createElement("li");
+	li.dataset.toDoIndex;
+
+	const checkbox = document.createElement("input");
+	checkbox.setAttribute("type", "checkbox");
+	checkbox.addEventListener("click", strikeThroughText);
+	li.appendChild(checkbox);
+
+	const p = document.createElement("p");
+	p.classList.add("todo-text");
+	p.textContent = toDoText;
+	li.appendChild(p);
+
+	const removeButton = document.createElement("span");
+	removeButton.classList.add("remove-button");
+	removeButton.textContent = "✖";
+	removeButton.addEventListener("click", (e) => e.target.parentNode.remove());
+	li.appendChild(removeButton);
+
+	ul.appendChild(li);
+}
+
+function strikeThroughText(e) {
+	e.target.nextSibling.style.textDecoration = e.target.checked
+		? "line-through"
+		: "";
+}
+
+// Randomize Quote and Add Quote and apply immediately(?)
+const quoteArr = [];
 
 // // Window On Click
 // window.addEventListener("click", windowOnClick);
